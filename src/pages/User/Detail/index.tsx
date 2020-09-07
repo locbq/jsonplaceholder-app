441605bdf72983639bf9416bf567a05ecd77661e
lucasbui <locbq.cyclonedart@gmail.com>
2020-09-07 21:05:38 +0700

    user detail

--- src/pages/User/Detail/index.tsx
+++ src/pages/User/Detail/index.tsx
@@ -8,6 +8,17 @@ import { Row, Col, Typography } from 'antd';
 
 const { Text, Paragraph } = Typography;
 
+type Address = {
+  street: string,
+  suite: string,
+  city: string,
+  zipcode: string,
+  geo: {
+    lat: string,
+    lng: string,
+  },
+};
+
 const UserDetail: FC = () => {
   const [user, setUser] = useState<User>({
     id: Number(''),
@@ -46,34 +57,66 @@ const UserDetail: FC = () => {
     getUser();
   }, [id]);
 
+  const getAddress = (address: Address) => `${address.suite}, ${address.street} Street, ${address.city}, ${address.zipcode}`;
+
   return (
     <>
-      <Row justify="center">
+      <Row>
+        <Col md={6}>
+          <Text strong>Name:</Text>
+        </Col>
         <Col md={18}>
-          <Row>
-            <Col md={4}>
-              <Text strong>Name:</Text>
-            </Col>
-            <Col md={20}>
-              <Paragraph>{user.name}</Paragraph>
-            </Col>
-          </Row>
-          <Row>
-            <Col md={4}>
-              <Text strong>Username:</Text>
-            </Col>
-            <Col md={20}>
-              <Paragraph>{user.username}</Paragraph>
-            </Col>
-          </Row>
-          <Row>
-            <Col md={4}>
-              <Text strong>Phone:</Text>
-            </Col>
-            <Col md={20}>
-              <Paragraph>{user.phone}</Paragraph>
-            </Col>
-          </Row>
+          <Paragraph>{user.name}</Paragraph>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={6}>
+          <Text strong>Username:</Text>
+        </Col>
+        <Col md={18}>
+          <Paragraph>{user.username}</Paragraph>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={6}>
+          <Text strong>Phone:</Text>
+        </Col>
+        <Col md={18}>
+          <Paragraph>{user.phone}</Paragraph>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={6}>
+          <Text strong>Address:</Text>
+        </Col>
+        <Col md={18}>
+          <Paragraph>{getAddress(user.address)}</Paragraph>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={6}>
+          <Text strong>Website:</Text>
+        </Col>
+        <Col md={18}>
+          <Paragraph>
+            <a href={user.website}>{user.website}</a>
+          </Paragraph>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={6}>
+          <Text strong>Company:</Text>
+        </Col>
+        <Col md={18}>
+          <Paragraph>{user.company.name}</Paragraph>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={6}>
+          <Text strong>Company catchphrase:</Text>
+        </Col>
+        <Col md={18}>
+          <Paragraph>{user.company.catchPhrase}</Paragraph>
         </Col>
       </Row>
     </>
