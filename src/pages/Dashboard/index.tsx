b34aca5f51ddd99cd42a46255c582126a63f721a
lucasbui <locbq.cyclonedart@gmail.com>
2020-10-08 11:00:16 +0700

    dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,116 @@
-import React, { FC } from 'react';
+import React, {
+  FC,
+  useState,
+  useEffect,
+} from 'react';
+import {
+  Space,
+  Typography,
+} from 'antd';
+import { Link } from 'react-router-dom';
 
-const Dashboard: FC = () => (
-  <><h1>Dashboard</h1></>
-);
+import { getAlbumList } from 'apis/album/album';
+import { getCommentList } from 'apis/comment/comment';
+import { getPostList } from 'apis/post/post';
+import { getUserList } from 'apis/user/user';
+import {
+  StyledCard,
+  StyledTitle,
+} from './styles';
+
+const {
+  Paragraph,
+} = Typography;
+
+const Dashboard: FC = () => {
+  const [figures, setFigures] = useState({
+    user: 0,
+    post: 0,
+    comment: 0,
+    album: 0,
+  });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true);
+      try {
+        const albumRes = await getAlbumList();
+        const userRes = await getUserList();
+        const postRes = await getPostList();
+        const commentRes = await getCommentList();
+        setFigures({
+          user: userRes.data.length,
+          comment: commentRes.data.length,
+          post: postRes.data.length,
+          album: albumRes.data.length,
+        });
+      } catch (e) {
+        console.log(e);
+      }
+      setIsLoading(false);
+    };
+    getData();
+  }, []);
+
+  return (
+    <>
+      <Space>
+        <StyledCard
+          loading={isLoading}
+          title="User"
+          extra={<Link to="/user">More</Link>}
+        >
+          <Paragraph>Total</Paragraph>
+          <StyledTitle
+            level={2}
+            type="success"
+          >
+            {figures.user}
+          </StyledTitle>
+        </StyledCard>
+
+        <StyledCard
+          loading={isLoading}
+          title="Post"
+          extra={<Link to="/post">More</Link>}
+        >
+          <Paragraph>Total</Paragraph>
+          <StyledTitle
+            level={2}
+            type="danger"
+          >
+            {figures.post}
+          </StyledTitle>
+        </StyledCard>
+
+        <StyledCard
+          loading={isLoading}
+          title="Comment"
+          extra={<Link to="/comment">More</Link>}
+        >
+          <Paragraph>Total</Paragraph>
+          <StyledTitle
+            level={2}
+            type="warning"
+          >
+            {figures.comment}
+          </StyledTitle>
+        </StyledCard>
+
+        <StyledCard
+          loading={isLoading}
+          title="Album"
+          extra={<Link to="/album">More</Link>}
+        >
+          <Paragraph>Total</Paragraph>
+          <StyledTitle level={2}>
+            {figures.album}
+          </StyledTitle>
+        </StyledCard>
+      </Space>
+    </>
+  );
+};
 
 export default Dashboard;
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import {
+  Card,
+  Typography,
+} from 'antd';
+
+const {
+  Title,
+} = Typography;
+
+export const StyledCard = styled(Card)`
+  width: 300px;
+  margin-right: 1rem;
+`;
+
+export const StyledTitle = styled(Title)`
+  margin: 0px !important;
+`;
